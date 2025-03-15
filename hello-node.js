e54136bfca71565be840f8875ea06c54ea915fd5
jmallmite <jmallmite@gmail.com>
2025-03-16 00:32:10 +0900

    Create hello-node.js

---
+++ hello-node.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+let count = 0;
+
+// req : request / 요청
+// res : response / 응답
+const server = http.createServer((req, res) => {
+    console.log((count += 1));
+    res.statusCode = 200;
+    res.setHeader("Conetent-type", "text/plain");
+    res.write("hahahaa\n");
+
+    setTimeout(() => {
+        res.end("hohoho");
+    }, 2000);
+});
+
+server.listen(8000);
